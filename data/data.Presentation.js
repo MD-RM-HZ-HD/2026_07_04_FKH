25d48780812cf40fe4f556c1a1b8f5522dea40a8
HZHD25 <6340523@gmail.com>
2026-07-01 01:31:18 +0300

    Update data.Presentation.js

--- data/data.Presentation.js
+++ data/data.Presentation.js
@@ -1,13 +1,13 @@
 const Presentation_Data = [
-    { title: "المسائل 1344-1349", audio: "Audio/1344-1349.m4a", pdf: "PDF/1344-1349.pdf", info: "info/1344-1349.png" },
-    { title: "المسألة 1350", audio: "Audio/1350.m4a", pdf: "PDF/1350.pdf", info: "info/1350.png" },
-    { title: "المسائل 1351-1352", audio: "Audio/1351-1352.m4a", pdf: "PDF/1351-1352.pdf", info: "info/1351-1352.png" },
-    { title: "المسائل 1353-1355", audio: "Audio/1353-1355.m4a", pdf: "PDF/1353-1355.pdf", info: "info/1353-1355.png" },
-    { title: "المسائل 1356-1357", audio: "Audio/1356-1357.m4a", pdf: "PDF/1356-1357.pdf", info: "info/1356-1357.png" },
-    { title: "المسائل 1358-1361", audio: "Audio/1358-1361.m4a", pdf: "PDF/1358-1361.pdf", info: "info/1358-1361.png" },
-    { title: "المسائل 1362-1367", audio: "Audio/1362-1367.m4a", pdf: "PDF/1362-1367.pdf", info: "info/1362-1367.png" },
-    { title: "المسائل 1368-1375", audio: "Audio/1368-1375.m4a", pdf: "PDF/1368-1375.pdf", info: "info/1368-1375.png" },
-    { title: "مسائل أعمال البنوك والمصارف", audio: "Audio/banks.m4a", pdf: "PDF/banks.pdf", info: "info/banks.png" },
-    { title: "مسائل الاعتمادات", audio: "Audio/credits.m4a", pdf: "PDF/credits.pdf", info: "info/credits.png" },
-    { title: "مسائل البضائع", audio: "Audio/goods.m4a", pdf: "PDF/goods.pdf", info: "info/goods.png" }
+    { title: "المسائل 1344-1349", audio: "Audio/1344-1349.mp3", pdf: "PDF/1344-1349.pdf", info: "info/1344-1349.png" },
+    { title: "المسألة 1350", audio: "Audio/1350.mp3", pdf: "PDF/1350.pdf", info: "info/1350.png" },
+    { title: "المسائل 1351-1352", audio: "Audio/1351-1352.mp3", pdf: "PDF/1351-1352.pdf", info: "info/1351-1352.png" },
+    { title: "المسائل 1353-1355", audio: "Audio/1353-1355.mp3", pdf: "PDF/1353-1355.pdf", info: "info/1353-1355.png" },
+    { title: "المسائل 1356-1357", audio: "Audio/1356-1357.mp3", pdf: "PDF/1356-1357.pdf", info: "info/1356-1357.png" },
+    { title: "المسائل 1358-1361", audio: "Audio/1358-1361.mp3", pdf: "PDF/1358-1361.pdf", info: "info/1358-1361.png" },
+    { title: "المسائل 1362-1367", audio: "Audio/1362-1367.mp3", pdf: "PDF/1362-1367.pdf", info: "info/1362-1367.png" },
+    { title: "المسائل 1368-1375", audio: "Audio/1368-1375.mp3", pdf: "PDF/1368-1375.pdf", info: "info/1368-1375.png" },
+    { title: "مسائل أعمال البنوك والمصارف", audio: "Audio/banks.mp3", pdf: "PDF/banks.pdf", info: "info/banks.png" },
+    { title: "مسائل الاعتمادات", audio: "Audio/credits.mp3", pdf: "PDF/credits.pdf", info: "info/credits.png" },
+    { title: "مسائل البضائع", audio: "Audio/goods.mp3", pdf: "PDF/goods.pdf", info: "info/goods.png" }
 ];
